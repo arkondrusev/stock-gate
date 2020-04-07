4cbc32c15a2f4b94216511336a5fb0948faa70f0
artem.kondrusev@gmail.com <Sigourney714>
2020-04-07 14:30:19 +0300

    + парсинг баланса с hitbtc. юнит-тест + получение баланса с hitbtc адаптера. юнит-тест

--- dev/src/main/java/com/sadkoala/stockgate/adapter/HitbtcAdapter.java
+++ dev/src/main/java/com/sadkoala/stockgate/adapter/HitbtcAdapter.java
@@ -5,13 +5,21 @@ import com.sadkoala.stockgate.communicator.HitbtcCommunicator;
 import com.sadkoala.stockgate.parser.HitbtcParser;
 import com.sadkoala.stockgate.parser.model.Order;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class HitbtcAdapter extends AbstractStockAdapter {
 
     public static List<Order> getOpenOrders(String symbol) throws Exception {
         GateUtils.checkParamNotEmpty(symbol, "symbol");
+
         return HitbtcParser.parseOpenOrders(HitbtcCommunicator.requestOpenOrders(symbol));
+    }
+
+    public static BigDecimal getCurrencyBalanceAvailaible(String currency) throws Exception {
+        GateUtils.checkParamNotEmpty(currency, "currency");
+
+        return HitbtcParser.parseTradingBalanceAvailaible(HitbtcCommunicator.requestTradingBalance(), currency);
     }
 
 }
--- dev/src/test/java/com/sadkoala/stockgate/adapter/HitbtcAdapterTest.java
+++ dev/src/test/java/com/sadkoala/stockgate/adapter/HitbtcAdapterTest.java
@@ -3,6 +3,7 @@ package com.sadkoala.stockgate.adapter;
 import com.sadkoala.stockgate.parser.model.Order;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class HitbtcAdapterTest {
@@ -11,6 +12,12 @@ public class HitbtcAdapterTest {
     public void testGetOpenOrders() throws Exception {
         List<Order> orders = HitbtcAdapter.getOpenOrders("BTCUSD");
         System.out.println("open orders count = " + orders.size());
+    }
+
+    @Test
+    public void testGetCurrencyBalanceAvailable() throws Exception {
+        BigDecimal balance = HitbtcAdapter.getCurrencyBalanceAvailaible("USD");
+        System.out.println("balance = " + balance.toPlainString());
     }
 
 }
