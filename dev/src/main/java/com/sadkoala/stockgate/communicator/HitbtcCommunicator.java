2ce9084ad19d9f6f3f36c9a9d21046b55ea8595e
artem.kondrusev@gmail.com <Sigourney714>
2020-04-07 22:33:53 +0300

    + получение стаканов с хитбтс. юнит-тест

--- dev/src/main/java/com/sadkoala/stockgate/communicator/HitbtcCommunicator.java
+++ dev/src/main/java/com/sadkoala/stockgate/communicator/HitbtcCommunicator.java
@@ -3,7 +3,9 @@ package com.sadkoala.stockgate.communicator;
 import com.sadkoala.httpscommunicator.HttpsCommunicator;
 import com.sadkoala.stockgate.GateUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HitbtcCommunicator extends AbstractStockCommunicator {
@@ -88,6 +90,61 @@ public class HitbtcCommunicator extends AbstractStockCommunicator {
      */
     public static String requestTradingBalance() throws Exception {
         return requestWithAuthorization("/api/2/trading/balance");
+    }
+
+    /**
+     * #### Orderbook
+     *
+     * `GET /api/2/public/orderbook`
+     *
+     * An Order Book is an electronic list of buy and sell orders for a specific symbol, structured by price level.
+     *
+     * You can optionally use comma-separated list of symbols. If it is not provided, null or empty, the request returns an Order Book for all symbols.
+     *
+     * Requires no API key Access Rights.
+     *
+     * Parameters:
+     *
+     * Name	Type	Description
+     * limit	Number	Limit of Order Book levels
+     * Default value: 100
+     * Set 0 to view full list of Order Book levels.
+     * symbols	String	Comma-separated list of symbol codes. Optional parameter
+     * Responses:
+     *
+     * Name	Type	Description
+     * ask	Array	Ask side array of levels
+     * bid	Array	Bid side array of levels
+     * size	Number	Total volume of orders with the specified price
+     * price	Number	Price level
+     *
+     * ```
+     */
+    public static String requestOrderbook(List<String> symbols, int limit) throws Exception {
+        StringBuilder urlString = new StringBuilder(HOST);
+        urlString.append("/api/2/public/orderbook");
+        StringBuilder symbolsList = new StringBuilder();
+        int i = 0;
+        for (String symbol : symbols) {
+            symbolsList.append(symbol);
+            if (symbols.size()-1 > i) { // if current symbol not last
+                symbolsList.append(",");
+            }
+        }
+        String symbolsParam = symbolsList.toString();
+        if (!symbolsParam.isBlank()) {
+            urlString.append("?").append("symbols=").append(symbolsList);
+        }
+        if (limit >=0) {
+            urlString.append("&").append("limit=").append(limit);
+        }
+        return HttpsCommunicator.executeHttpsRequest(urlString.toString());
+    }
+
+    public static String requestOrderbook(String symbol, int limit) throws Exception {
+        List<String> symbols = new ArrayList<>();
+        symbols.add(symbol);
+        return requestOrderbook(symbols, limit);
     }
 
     private static String requestWithAuthorization(final String endpoint, final String requestParams) throws Exception {
--- dev/src/test/java/com/sadkoala/stockgate/communicator/HitbtcCommunicatorTest.java
+++ dev/src/test/java/com/sadkoala/stockgate/communicator/HitbtcCommunicatorTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 
 public class HitbtcCommunicatorTest {
 
+    private static final String BTC_USD_SYMBOL = "BTCUSD";
+
     @Test
     public void testRequestOpenOrders() throws Exception {
-        String response = HitbtcCommunicator.requestOpenOrders("BTCUSD");
+        String response = HitbtcCommunicator.requestOpenOrders(BTC_USD_SYMBOL);
         System.out.println(response);
     }
 
@@ -15,5 +17,12 @@ public class HitbtcCommunicatorTest {
         String response = HitbtcCommunicator.requestTradingBalance();
         System.out.println(response);
     }
+
+    @Test
+    public void testRequestOrderbook() throws Exception {
+        String response = HitbtcCommunicator.requestOrderbook(BTC_USD_SYMBOL, 5);
+        System.out.println(response);
+    }
+
 
 }
