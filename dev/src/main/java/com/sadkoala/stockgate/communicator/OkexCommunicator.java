f6da1afff5432a2edc9cb9554ff7675dfa4b0b5d
artem.kondrusev@gmail.com <Sigourney714>
2020-04-21 23:56:34 +0300

    + размещение ордера на okex

--- dev/src/main/java/com/sadkoala/stockgate/communicator/OkexCommunicator.java
+++ dev/src/main/java/com/sadkoala/stockgate/communicator/OkexCommunicator.java
@@ -1,8 +1,12 @@
 package com.sadkoala.stockgate.communicator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sadkoala.httpscommunicator.HttpsCommunicator;
 import com.sadkoala.stockgate.GateUtils;
 
+import java.math.BigDecimal;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
@@ -17,81 +21,18 @@ public class OkexCommunicator extends AbstractStockCommunicator {
 
     private static final String HOST = "www.okex.com";
 
-    /**
-     * Open Orders
-     * This retrieves the list of your current open orders. Pagination is supported and the response is sorted with most recent first in reverse chronological order.
-     *
-     * Rate limit: 20 requests per 2 seconds
-     * HTTP Request
-     * GET/api/spot/v3/orders_pending
-     *
-     * Example Request
-     * 2018-09-12T07:51:51.138ZGET/api/spot/v3/orders_pending?limit=2&instrument_id=btc-usdt&after=2500723297223680
-     *
-     * Parameters
-     * Parameter	Type	Required	Description
-     * instrument_id	String	Yes	Trading pair symbol
-     * after	String	No	Pagination of data to return records earlier than the requested order_id.
-     * before	String	No	Pagination of data to return records newer than the requested order_id.
-     * limit	String	No	Number of results per request. The maximum is 100; the default is 100
-     */
+    private static ObjectMapper mapper = new ObjectMapper();
+
     public static String requestOpenOrders(String symbol) throws Exception {
         GateUtils.checkParamNotEmpty(symbol, "symbol");
 
         return requestWithAuthorization("/api/spot/v3/orders_pending", "instrument_id=" + symbol);
     }
 
-    /**
-     * Account Information
-     * This retrieves the list of assets, (with non-zero balance), remaining balance, and amount available in the spot trading account.
-     *
-     * Rate limit: 20 requests per 2 seconds
-     * HTTP Requests
-     * GET/api/spot/v3/accounts
-     *
-     * Example Request
-     * GET/api/spot/v3/accounts
-     *
-     * Return Parameters
-     * Parameters	Parameters Types	Description
-     * currency	String	Token symbol
-     * balance	String	Remaining balance
-     * id	String	Account ID
-     * hold	String	Amount on hold (not available)
-     * available	String	Available amount
-     * Notes
-     * After placing an order, the order amount will be put on hold. After you placed an order, the amount of the order will be put on hold. You will not be able to transfer or use in other orders until the order is completed or cancelled.
-     */
     public static String requestBalances() throws Exception {
         return requestWithAuthorization("/api/spot/v3/accounts");
     }
 
-    /**
-     * Public - Order Book
-     * Retrieve a trading pair's order book. Pagination is not supported here; the entire orderbook will be returned per request. This is publicly accessible without account authentication. WebSocket is recommended here.
-     *
-     * Rate limit: 20 requests per 2 seconds
-     * HTTP Request
-     * GET/api/spot/v3/instruments/<instrument_id>/book
-     *
-     * Example Request
-     * GET/api/spot/v3/instruments/BTC-USDT/book?size=5&depth=0.2
-     *
-     * Parameters
-     * Parameter	Type	Required	Description
-     * size	String	No	Number of results per request. Maximum 200
-     * depth	String	No	Aggregation of the order book. e.g . 0.1, 0.001
-     * instrument_id	String	No	Trading pair symbol
-     * Response
-     * Parameter	Type	Description
-     * asks	List<String>	Sell side depth
-     * bids	List<String>	Buy side depth
-     * timestamp	String	timestamp
-     * Notes
-     * Aggregation of the order book means that orders within a certain price range is combined and considered as one order cluster.
-     *
-     * When size is not passed in the parameters, one entry is returned; when size is 0, no entry is returned. The maximum size is 200. When requesting more than 200 entries, at most 200 entries are returned.
-     */
     public static String requestOrderbook(final String symbol, final Integer limit, final Float depth) throws Exception {
         GateUtils.checkParamNotEmpty(symbol, "symbol");
 
@@ -102,6 +43,35 @@ public class OkexCommunicator extends AbstractStockCommunicator {
                                 .addParamIfNotEmpty("size", limit)
                                 .addParamIfNotEmpty("depth", depth)
                                 .build()));
+    }
+
+    public static String requestNewOrder(final String symbol, final String side, final String type, final BigDecimal qty, final BigDecimal price) throws Exception {
+        GateUtils.checkParamNotEmpty(symbol, "symbol");
+        GateUtils.checkParamNotEmpty(side, "side");
+        GateUtils.checkParamNotEmpty(type, "type");
+        GateUtils.checkParamNotNull(qty, "qty");
+
+        return requestPostWithAuthorization("/api/spot/v3/orders",
+                makeNewOrderRequestContent(symbol, side, type, qty.toPlainString(), price.toPlainString()));
+    }
+
+    private static String makeNewOrderRequestContent(final String symbol, final String side, final String type, final String qty, final String price) throws JsonProcessingException {
+        ObjectNode root = mapper.createObjectNode();
+        root.put("instrument_id", symbol);
+        root.put("side", side);
+        root.put("type", type);
+        if ("limit".equals(type)) {
+            root.put("size", qty);
+            root.put("price", price);
+        } else {
+            if ("buy".equals(side)) {
+                root.put("notional", qty);
+            } else {
+                root.put("size", qty);
+            }
+        }
+
+        return mapper.writeValueAsString(root);
     }
 
     private static String requestWithAuthorization(final String endpoint, final String params) throws Exception {
@@ -127,6 +97,21 @@ public class OkexCommunicator extends AbstractStockCommunicator {
         GateUtils.checkParamNotEmpty(endpoint, "endpoint");
 
         return requestWithAuthorization(endpoint, EMPTY_STRING);
+    }
+
+    private static String requestPostWithAuthorization(final String endpoint, final String requestContent) throws Exception {
+        GateUtils.checkParamNotEmpty(endpoint, "endpoint");
+        GateUtils.checkParamNotNull(requestContent, "requestContent");
+
+        String timestamp = getUnixTime();
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json; charset=UTF-8");
+        headers.put("Accept", "application/json");
+        headers.put("OK-ACCESS-KEY", API_KEY_VALUE);
+        headers.put("OK-ACCESS-SIGN", makeSign(timestamp + "POST" + endpoint + requestContent));
+        headers.put("OK-ACCESS-TIMESTAMP", timestamp);
+        headers.put("OK-ACCESS-PASSPHRASE", PASSPHRASE_VALUE);
+        return HttpsCommunicator.executePostRequest(HOST + endpoint, requestContent, headers);
     }
 
     private static String makeSign(String textToSign) throws InvalidKeyException, NoSuchAlgorithmException {
--- dev/src/test/java/com/sadkoala/stockgate/communicator/OkexCommunicatorTest.java
+++ dev/src/test/java/com/sadkoala/stockgate/communicator/OkexCommunicatorTest.java
@@ -1,12 +1,17 @@
 package com.sadkoala.stockgate.communicator;
 
+import com.sadkoala.stockgate.adapter.OkexAdapter;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 public class OkexCommunicatorTest {
 
+    private static final String BTC_USDT_SYMBOL = "btc-usdt";
+
     @Test
     public void testRequestOpenOrders() throws Exception {
-        String orders = OkexCommunicator.requestOpenOrders("btc-usdt");
+        String orders = OkexCommunicator.requestOpenOrders(BTC_USDT_SYMBOL);
         System.out.println(orders);
     }
 
@@ -18,8 +23,32 @@ public class OkexCommunicatorTest {
 
     @Test
     public void testRequestOrderbook() throws Exception {
-        String response = OkexCommunicator.requestOrderbook("BTC-USDT", 3, null);
+        String response = OkexCommunicator.requestOrderbook(BTC_USDT_SYMBOL, 3, null);
         System.out.println(response);
     }
+
+    @Test
+    public void testRequestNewOrder_CheckOrder_CancelOrder() throws Exception {
+        BigDecimal btcPrice = OkexAdapter.getOrderbook(BTC_USDT_SYMBOL, 1).getBid().getList().get(0).getPrice();
+
+        String resp = OkexCommunicator.requestNewOrder(BTC_USDT_SYMBOL, "buy", "limit", new BigDecimal("0.002"), btcPrice.subtract(new BigDecimal("1000")));
+        System.out.println(resp);
+//        Assertions.assertTrue(resp.contains("\"status\":\"NEW\""));
+//        String orderId = resp.substring(resp.indexOf("\"clientOrderId\":\"")+"\"clientOrderId\":\"".length(),resp.indexOf("\",\"transactTime\""));
+
+//        resp = BinanceCommunicator.requestCheckOrderStatus(BTC_USDT_SYMBOL, orderId);
+//        System.out.println(resp);
+//        Assertions.assertTrue(resp.contains("\"status\":\"NEW\""));
+
+
+//        resp = BinanceCommunicator.requestCancelOrder(BTC_USDT_SYMBOL, orderId);
+//        System.out.println(resp);
+//        Assertions.assertTrue(resp.contains("\"status\":\"CANCELED\""));
+    }
+
+//    @Test
+//    public void testMakeContent() throws JsonProcessingException {
+//        System.out.println(OkexCommunicator.makeNewOrderRequestContent(BTC_USDT_SYMBOL, "buy", "limit", "0.002", "7000"));
+//    }
 
 }
